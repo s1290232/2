29f96009a91a86d2c87ff70c1408df4d7412edd5
s1290232 <109269809+s1290232@users.noreply.github.com>
2022-07-14 22:42:58 +0900

    user name added

--- ex04.java
+++ ex04.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class DM4{
 public static void main (String[] args)
 {
@@ -6,6 +8,13 @@ Random r=new Random();
 int ran;
 int cnt1=0;
 int cnt2=0;
+  String name;
+  Scanner sc=new Scanner(System.in);
+  
+  System.out.println("Who are you?");
+  name=sc.next();
+  System.out.println("Hello, "+name+"!");
+  
 System.out.println("Tossing a coin ...");
 for(int i=1;i<=3;i++){
 ran=r.nextInt(2);
@@ -22,6 +31,6 @@ break;
 }
 }
 System.out.println(Heads: "+cnt1+", Tails: "+cnt2);
-
+               
 }
 }
